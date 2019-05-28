57a0e57c1113a4ace0a0c7eee25fca42a6782210
748674 <748674@123-PC17.student.Tor.Loc>
2019-05-28 16:19:22 -0400

    Q1: The total number of characters in the sentence

---
+++ quizz3rd/src/quizz3rd/third.java
@@ -0,0 +1,18 @@
+package quizz3rd;
+
+public class third {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create a string
+				String name= "copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss";
+				
+				//number of characters in the string
+				int numberofcharactrs=name.length();
+				System.out.println("number of characters:"+numberofcharactrs);
+
+
+
+	}
+
+}
