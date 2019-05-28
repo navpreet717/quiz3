fa5307e24c6d30fa23ba0cf74b8b89ea0797673b
748674 <748674@123-PC17.student.Tor.Loc>
2019-05-28 16:27:31 -0400

    //The last 15 characters in the string

--- quizz3rd/src/quizz3rd/third.java
+++ quizz3rd/src/quizz3rd/third.java
@@ -11,7 +11,12 @@ public class third {
 				int numberofcharactrs=name.length();
 				System.out.println("number of characters:"+numberofcharactrs);
 
-
+//The last 15 characters in the string
+				System.out.println("last 15 characters: " +
+                        name.substring(name.length() - 15));		
+				
+				
+				//the number of times appear in the sentence COMMIT YOUR CODE
 
 	}
 
