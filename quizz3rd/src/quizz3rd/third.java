e212c9868354f7740b4786fd9e98afbf78354211
748674 <748674@123-PC17.student.Tor.Loc>
2019-05-28 16:49:10 -0400

    The number of times “c” and “p” appear in the sentence

--- quizz3rd/src/quizz3rd/third.java
+++ quizz3rd/src/quizz3rd/third.java
@@ -1,5 +1,7 @@
 package quizz3rd;
 
+import java.util.HashMap;
+
 public class third {
 
 	public static void main(String[] args) {
@@ -16,8 +18,24 @@ public class third {
                         name.substring(name.length() - 15));		
 				
 				
-				//the number of times appear in the sentence COMMIT YOUR CODE
-
-	}
-
-}
+				//the number of times  c appear in the sentence 
+				int charCount = 0;
+		        for(int i =0 ; i<name.length(); i++){
+		            if(name.charAt(i) == 'c'){
+		                charCount++;
+		            }
+		            
+		        }
+		        System.out.println(" the number of times c appear in the sentence  " + charCount);
+		      //the number of times  p appear in the sentence 
+				int charCount1 = 0;
+		        for(int i =0 ; i<name.length(); i++){
+		            if(name.charAt(i) == 'p'){
+		                charCount1++;
+		            }
+		            
+		        }
+		        System.out.println(" the number of times p appear in the sentence  " + charCount1);
+		        
+		      }}
+		       
